e3074f46e3bdb501297674ca06a9e86ce480d8e0
Barbero10 <Riusbabrero@gmail.com>
2026-04-16 14:34:48 -0300

    actividad 3

---
+++ actividad3/index.mjs
@@ -0,0 +1,64 @@
+import http from 'node:http'
+import fsp from 'node:fs/promises'
+import path from 'node:path'
+const PORT = 3000
+const filePath = path.join('./usuarios.json')
+
+const app = http.createServer(async (peticion, respuesta) => {
+
+  try {
+
+    if (peticion.method === 'GET') {
+
+     
+      if (peticion.url === '/usuarios') {
+
+        const respuesta = await fetch('https://api.escuelajs.co/api/v1/users')
+        const datos = await respuesta.json()
+
+        await fsp.writeFile(filePath, JSON.stringify(datos, null, 2))
+
+        const archivo = await fsp.readFile(filePath, 'utf-8')
+
+        respuesta.statusCode = 200
+        
+        respuesta.setHeader('Content-Type', 'application/json')
+        return respuesta.end(archivo)
+      }
+
+      
+      if (peticion.url === '/usuarios/filtrados.') {
+        try {
+          const archivo = await fsp.readFile(filePath, 'utf-8')
+          const usuarios = JSON.parse(archivo)
+
+          const filtrados = usuarios.filter(u => u.id < 10)
+
+          respuesta.statusCode = 200
+
+
+          respuesta.setHeader('Content-Type', 'application/json')
+
+          return respuesta.end('filtrados')
+
+        } catch (error) {
+          respuesta.statusCode = 400
+          return respuesta.end('Primero ejecutar /usuarios')
+        }
+      }
+    }
+
+    // fallback
+    respuesta.statusCode = 404
+    respuesta.end('Recurso no encontrado')
+
+  } catch (error) {
+    respuesta.statusCode = 500
+    respuesta.end('Error interno del servidor')
+  }
+
+})
+
+app.listen(PORT, () => {
+  console.log(`Servidor en http://localhost:${PORT}`)
+})
